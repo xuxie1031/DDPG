8ff70657d8d8e7dc05f1dfb68468e783e7e97e2e
xuxie1031 <404839856@qq.com>
2018-08-07 18:36:23 -0700

    ddpg full

---
+++ config.py
@@ -0,0 +1,10 @@
+class Config:
+    def __init__(self):
+        self.task_fn = None
+        self.network_fn = None
+        self.replay_fn = None
+        self.random_process_fn = None
+        self.episodes_num = 0
+        self.target_network_mix = 0
+        self.min_replay_size = 0
+        self.discount = 0
---
+++ main.py
@@ -0,0 +1,34 @@
+import torch
+
+from task import *
+from replay import *
+from randomProcess import *
+from nnUtils import *
+from nnDDPG import DDPGNet
+from config import Config
+
+from agentDDPG import DDPGAgent
+
+
+def run_ddpg():
+    config = Config()
+    config.task_fn = lambda name: Roboschool(name)
+    config.network_fn = lambda state_dim, action_dim: DDPGNet(
+        state_dim, action_dim,
+        actor_body = FCBody(state_dim, hidden_units=(300, 200), gate=F.tanh),
+        critic_body = FCBodyWithAction(state_dim, action_dim, hidden_state_dim=400, hidden_units=(300, ), gate=F.tanh),
+        actor_opt_fn = lambda params: torch.optim.Adam(params, lr=1e-4),
+        critic_opt_fn = lambda params: torch.optim.Adam(params, lr=1e-3)
+    )
+    config.replay_fn = Replay(memory_size=1000000, batch_size=64)
+    config.discount = .99
+    config.random_process_fn = lambda action_dim: OrnsteinUhlenbeckProcess(size=(action_dim, ), std=LinearSchedule(.2))
+    config.min_replay_size = 64
+    config.target_network_mix = 1e-3
+
+    agent = DDPGAgent(config)
+    agent.run_agent()
+
+
+if __name__ == '__main__':
+    run_ddpg()
